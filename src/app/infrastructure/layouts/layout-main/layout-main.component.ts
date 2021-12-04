ea5825569013269b3e4e3f25e83d3bad00edb6ec
Amin Nateghi <amin.programmer@gmail.com>
2021-12-04 22:50:10 +0300

    added layout main

---
+++ src/app/infrastructure/layouts/layout-main/layout-main.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-main',
+  templateUrl: './layout-main.component.html',
+})
+export class LayoutMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
