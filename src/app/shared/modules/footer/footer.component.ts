b969e2b9eab96bcaf9bcbf8b40f47ab154a81f5a
Amin Nateghi <amin.programmer@gmail.com>
2021-12-07 21:13:23 +0300

    change route method in footer link

--- src/app/shared/modules/footer/footer.component.ts
+++ src/app/shared/modules/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  gotoClick(path: string[]) {
+    this.router.navigate(path);
+  }
 }
