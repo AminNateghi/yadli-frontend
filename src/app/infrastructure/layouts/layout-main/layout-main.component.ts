184789fdc7a4fb3d24302131d24a30956d9e007c
Amin Nateghi <amin.programmer@gmail.com>
2021-12-12 00:11:23 +0300

    change main-layout to tailwind & added button css style

--- src/app/infrastructure/layouts/layout-main/layout-main.component.ts
+++ src/app/infrastructure/layouts/layout-main/layout-main.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout-main',
   templateUrl: './layout-main.component.html',
+  styleUrls: ['./layout-main.component.scss']
 })
 export class LayoutMainComponent implements OnInit {
 
